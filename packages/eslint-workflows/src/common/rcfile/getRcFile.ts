d1ffeb397ec610981255ae025206a7e933241123
nossbigg <chenggibson@gmail.com>
2022-04-04 18:04:18 +1000

    Improve rc paths support (absolute/relative)

--- packages/eslint-workflows/src/common/rcfile/getRcFile.ts
+++ packages/eslint-workflows/src/common/rcfile/getRcFile.ts
@@ -1,14 +1,27 @@
-import path from "path";
+import path, { isAbsolute } from "path";
 import { RcFile } from "./typedefs";
 
 export const getRcFile = (): RcFile => {
   const rcFilePath = getRcFilePath();
-  const rcFile = require(rcFilePath);
-  return rcFile;
+  const rcFile: RcFile = require(rcFilePath);
+
+  const modifiedRcFile: RcFile = {
+    codeownersPath: handlePath(rcFile.codeownersPath),
+    eslintOutputPath: handlePath(rcFile.eslintOutputPath),
+  };
+  return modifiedRcFile;
 };
 
 const getRcFilePath = (): string => {
   const cwd = process.cwd();
   const filePath = path.join(cwd, "eslint-workflows", ".eslint-workflowsrc.js");
   return filePath;
+};
+
+const handlePath = (filePath: string): string => {
+  if (isAbsolute(filePath)) {
+    return filePath;
+  }
+  const cwd = process.cwd();
+  return path.join(cwd, filePath);
 };
