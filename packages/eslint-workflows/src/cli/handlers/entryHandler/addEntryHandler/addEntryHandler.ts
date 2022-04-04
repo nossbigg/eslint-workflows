a3e72d04456fd602dcc4ce3f1ac1ea1f98285a30
nossbigg <chenggibson@gmail.com>
2022-04-04 20:49:29 +1000

    [entry-add] Remove all console.log

--- packages/eslint-workflows/src/cli/handlers/entryHandler/addEntryHandler/addEntryHandler.ts
+++ packages/eslint-workflows/src/cli/handlers/entryHandler/addEntryHandler/addEntryHandler.ts
@@ -12,21 +12,16 @@ import { showSelectPrompt } from "../../../prompts";
 import { CommonConfig } from "../../../../common/rcfile/typedefs";
 
 export const addEntryHandler: CommandHandler = async () => {
-  console.log("entry - add");
-
   const commonConfig = getCommonConfig();
-  console.log(commonConfig);
-
   const { eslintOutput } = commonConfig;
-  const availableRuleIds = getAvailableRuleIds(eslintOutput);
 
+  const availableRuleIds = getAvailableRuleIds(eslintOutput);
   const selectedRuleId = await showSelectPrompt({
     message: "Select rule to add",
     options: availableRuleIds,
   });
 
   const newEntry = makeNewWorkflowEntry(commonConfig, selectedRuleId);
-  console.log(newEntry);
 };
 
 const makeNewWorkflowEntry = (
@@ -36,14 +31,9 @@ const makeNewWorkflowEntry = (
   const { eslintOutput, codeowners } = commonConfig;
 
   const matchingFileNames = getFilenamesByRuleId(eslintOutput, selectedRuleId);
-  console.log(matchingFileNames);
-
   const owners = getOwners(codeowners);
-  console.log(owners);
 
   const baseWorkflowEntry = makeBaseWorkflowEntry(selectedRuleId, owners);
-  console.log(baseWorkflowEntry);
-
   const withMatchedOwners = matchingFileNames.reduce((acc, fileName) => {
     const matchingOwner = getMatchingOwner(codeowners, fileName);
     const { owners } = matchingOwner;
@@ -54,7 +44,6 @@ const makeNewWorkflowEntry = (
 
     return acc;
   }, baseWorkflowEntry);
-  console.log(withMatchedOwners);
 
   return withMatchedOwners;
 };
