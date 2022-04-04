b8d15636b0d60586565f3929a4bedb40bb99948d
nossbigg <chenggibson@gmail.com>
2022-04-04 21:02:04 +1000

    Do not break lines in yml for long file names

--- packages/eslint-workflows/src/common/workflows-entries/saveWorkflowsEntries.ts
+++ packages/eslint-workflows/src/common/workflows-entries/saveWorkflowsEntries.ts
@@ -1,11 +1,16 @@
 import fs from "fs";
-import yaml from "js-yaml";
+import yaml, { DumpOptions } from "js-yaml";
 import { WorkflowEntries } from "./typedefs";
+
+const yamlDumpOpts: DumpOptions = {
+  // do not break lines for long file names
+  lineWidth: -1,
+};
 
 export const saveWorkflowsEntries = (
   filePath: string,
   workflowsEntries: WorkflowEntries
 ): void => {
-  const output = yaml.dump(workflowsEntries);
+  const output = yaml.dump(workflowsEntries, yamlDumpOpts);
   fs.writeFileSync(filePath, output);
 };
