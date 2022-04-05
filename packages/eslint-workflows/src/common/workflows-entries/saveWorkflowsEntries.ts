5955b30888f07e952ddec248a7d3190d586ed0fc
nossbigg <chenggibson@gmail.com>
2022-04-05 17:19:18 +1000

    Sort keys when saving yml

--- packages/eslint-workflows/src/common/workflows-entries/saveWorkflowsEntries.ts
+++ packages/eslint-workflows/src/common/workflows-entries/saveWorkflowsEntries.ts
@@ -5,6 +5,7 @@ import { WorkflowEntries } from "./typedefs";
 const yamlDumpOpts: DumpOptions = {
   // do not break lines for long file names
   lineWidth: -1,
+  sortKeys: true,
 };
 
 export const saveWorkflowsEntries = (
