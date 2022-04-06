12977a8e75d5109804d0067a4c1806ec13280de9
nossbigg <chenggibson@gmail.com>
2022-04-06 17:16:19 +1000

    [init] Refactor getCodeownersPath to own file

---
+++ packages/eslint-workflows/src/cli/commands/initCommand/getCodeownersPath.ts
@@ -0,0 +1,46 @@
+import fs from "fs-extra";
+import _ from "lodash";
+import { makePath } from "../../../common";
+import { showSelectPrompt } from "../../prompts";
+
+export const getCodeownersPath = async (
+  projectRoot: string
+): Promise<string | undefined> => {
+  const codeownersPaths = detectCodeownersFile(projectRoot);
+  if (codeownersPaths.length === 0) {
+    return undefined;
+  }
+
+  if (codeownersPaths.length === 1) {
+    return codeownersPaths[0];
+  }
+
+  console.log("Multiple CODEOWNERS files detected.");
+  const selectedPath = await showSelectPrompt({
+    message: "Select CODEOWNERS",
+    options: codeownersPaths,
+  });
+  return selectedPath;
+};
+
+const detectCodeownersFile = (projectRoot: string): string[] => {
+  const codeownersPaths = _.uniq([
+    // github
+    // ref: https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners
+    makePath("CODEOWNERS"),
+    makePath(".github", "CODEOWNERS"),
+    makePath("docs", "CODEOWNERS"),
+    // gitlab
+    // ref: https://docs.gitlab.com/ee/user/project/code_owners.html
+    makePath("CODEOWNERS"),
+    makePath(".gitlab", "CODEOWNERS"),
+    makePath("docs", "CODEOWNERS"),
+  ]);
+
+  const matchingPaths = codeownersPaths.filter((p) => {
+    const absPath = makePath(projectRoot, p);
+    const fileExists = fs.existsSync(absPath);
+    return fileExists;
+  });
+  return matchingPaths;
+};
--- packages/eslint-workflows/src/cli/commands/initCommand/initCommand.ts
+++ packages/eslint-workflows/src/cli/commands/initCommand/initCommand.ts
@@ -1,11 +1,9 @@
-import fs from "fs-extra";
 import { CommandHandler } from "../typedefs";
 import { makeConfigFiles } from "./makeConfigFiles";
 import { makePath } from "../../../common";
 import { RcFile } from "../../../common/rcfile/typedefs";
-import _ from "lodash";
-import { showSelectPrompt } from "../../prompts";
 import { showPostSetupMessages } from "./showPostSetupMessages";
+import { getCodeownersPath } from "./getCodeownersPath";
 
 export const initCommand: CommandHandler = async () => {
   // :CHECKS
@@ -28,48 +26,6 @@ export const initCommand: CommandHandler = async () => {
 
   makeConfigFiles(rcFilePath, defaultRcFile);
   showPostSetupMessages(defaultRcFile);
-};
-
-const getCodeownersPath = async (
-  projectRoot: string
-): Promise<string | undefined> => {
-  const codeownersPaths = detectCodeownersFile(projectRoot);
-  if (codeownersPaths.length === 0) {
-    return undefined;
-  }
-
-  if (codeownersPaths.length === 1) {
-    return codeownersPaths[0];
-  }
-
-  console.log("Multiple CODEOWNERS files detected.");
-  const selectedPath = await showSelectPrompt({
-    message: "Select CODEOWNERS",
-    options: codeownersPaths,
-  });
-  return selectedPath;
-};
-
-const detectCodeownersFile = (projectRoot: string): string[] => {
-  const pathsToLookAt = _.uniq([
-    // github
-    // ref: https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners
-    makePath("CODEOWNERS"),
-    makePath(".github", "CODEOWNERS"),
-    makePath("docs", "CODEOWNERS"),
-    // gitlab
-    // ref: https://docs.gitlab.com/ee/user/project/code_owners.html
-    makePath("CODEOWNERS"),
-    makePath(".gitlab", "CODEOWNERS"),
-    makePath("docs", "CODEOWNERS"),
-  ]);
-
-  const matchingPaths = pathsToLookAt.filter((p) => {
-    const absPath = makePath(projectRoot, p);
-    const fileExists = fs.existsSync(absPath);
-    return fileExists;
-  });
-  return matchingPaths;
 };
 
 const getProjectRoot = (): string => {
