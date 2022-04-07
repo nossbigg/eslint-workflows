8b640caaa9db685007651510357ae8e47a214b8b
nossbigg <chenggibson@gmail.com>
2022-04-07 19:38:37 +1000

    [entry-fmt] Add useful output messages

--- packages/eslint-workflows/src/cli/commands/entryCommand/fmtEntryCommand/fmtEntryCommand.ts
+++ packages/eslint-workflows/src/cli/commands/entryCommand/fmtEntryCommand/fmtEntryCommand.ts
@@ -1,8 +1,11 @@
 import { getCommonConfig, saveWorkflowsEntries } from "../../../../common";
+import { showChangesAppliedMessage } from "../../common";
 import { CommandHandler } from "../../typedefs";
 
 export const fmtEntryCommand: CommandHandler = async () => {
   const commonConfig = getCommonConfig();
   const { workflowsEntries, rcFile } = commonConfig;
+
   saveWorkflowsEntries(rcFile.workflowsEntriesPath, workflowsEntries);
+  showChangesAppliedMessage();
 };
